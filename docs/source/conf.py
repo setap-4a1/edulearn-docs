6fa5699045889765a29c80b95f99ce3f8154760b
George Busby <gbusby01@protonmail.com>
2026-03-06 08:59:16 +0000

    Update config with project name and author

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Edulearn'
+copyright = '2026, 4a1'
+author = '4a1'
 
 release = '0.1'
 version = '0.1.0'
